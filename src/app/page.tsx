02e590e50c29320c0e2303f5d617204ef26f4786
Madannadar <154582270+Madannadar@users.noreply.github.com>
2025-01-19 22:11:43 +0530

    almost completed the project in a day

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection";
+import MusicSchoolTestimonials from "@/components/TestimonialCard";
+import UpcomingWebinar from "@/components/UpcomingWebinar";
+import WhyChooseUs from "@/components/WhyChooseUs";
+
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+            {/* <h1 className="text-2xl text-center">chai aur code</h1> */}
+            <HeroSection/>
+            <FeaturedCourses/>
+            <WhyChooseUs/>
+            <MusicSchoolTestimonials/>
+            <UpcomingWebinar/>
+    </main>
+  );
+}
